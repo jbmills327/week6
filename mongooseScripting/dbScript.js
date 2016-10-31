07a97d0cc163bf7a319cece50d8db036579d460a
Justin Mills <jbmills@gmail.com>
2016-10-31 16:59:35 -0600

    finshed mongoose scripting exercise

---
+++ mongooseScripting/dbScript.js
@@ -0,0 +1,68 @@
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/zoo", (err) => {
+    if (err) {
+        console.log("Error: ", err);
+    } else {
+        console.log("Connected to the database!");
+    }
+});
+
+var newAnimals = [{
+    name: "Elephant",
+    diet: "People"
+}, {
+    name: "Donkey",
+    diet: "Also People"
+}, {
+    name: "Peacock",
+    diet: "You guessed it, people."
+}];
+
+var newVisitors = [{
+    name: "Hector",
+    favoriteAnimals: ["Puma", "Panther", "Duck"]
+}, {
+    name: "Samantha",
+    favoriteAnimals: ["Salamander", "Newt", "Bunny"]
+}, {
+    name: "Peter",
+    favoriteAnimals: ["Platypus", "One-Eyed Walrus", "Jackalope"]
+}];
+
+
+
+var animalsSchema = mongoose.Schema({
+    name: String,
+    diet: String,
+});
+
+var Animals = mongoose.model("Animals", animalsSchema);
+
+
+var visitorsSchema = mongoose.Schema({
+    name: String,
+    favoriteAnimals: Array
+
+});
+
+var Visitors = mongoose.model("Visitors", visitorsSchema);
+
+function createVisitor(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var visitor = new Visitors(arr[i]);
+        console.log("New visitor: ", visitor);
+        visitor.save();
+    }
+}
+
+function createAnimals(arr) {
+    for (var i = 0; i <= arr.length; i++) {
+        var animal = new Animals(arr[i]);
+        console.log("New animal: ", animal);
+        animal.save();
+    }
+}
+
+createVisitor(newVisitors);
+createAnimals(newAnimals);
---
+++ problemSetFour/myScripts.js
@@ -0,0 +1,10 @@
+function getNumber(word) {
+    var newArray = word.split("");
+    for (i = 0; i < newArray.length; i++) {
+
+    }
+    console.log(newNumber);
+}
+
+// console.log(getNumber("88hello8"));
+getNumber("88hello8")
